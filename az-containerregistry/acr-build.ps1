1fec17afc9d78109b3de187160f2d904ebe249e5
pirahawk <pirahawk@users.noreply.github.com>
2023-05-26 00:03:51 +0100

    update az-containerregistry

---
+++ az-containerregistry/acr-build.ps1
@@ -0,0 +1,11 @@
+# To run:  .\az-containerregistry\acr-build.ps1
+
+$acrGroupName=$(az group list --query "[?contains(name, 'az-containerregistry')].name" --output tsv)
+$acrName=$(az acr list -g $acrGroupName --query "[].loginServer" --output tsv)
+
+Write-Output "ACR location is: $acrName"
+
+# az acr login -n $acrName --expose-token
+
+# NOTE: I needed to set the context to the level of the Solution directory "AzContainerReg" not the Project because the dockerfile assumes it is at that level when it copies the .csproj file
+az acr build --image azcontainerreg/myapi:latest --registry $acrName --file .\az-containerregistry\AzContainerReg\AzContainerReg\Dockerfile .\az-containerregistry\AzContainerReg\
--- az-containerregistry/bicep/deploy.bicep
+++ az-containerregistry/bicep/deploy.bicep
@@ -1,4 +1,19 @@
 param resourceGroupName string
 param targetLocation string
 
+param containerRegistryName string
 
+resource containerRegistry 'Microsoft.ContainerRegistry/registries@2023-01-01-preview' = {
+  name: containerRegistryName
+  location: resourceGroup().location
+  sku: {
+    name: 'Standard'
+  }
+  properties:{
+    adminUserEnabled:true
+    anonymousPullEnabled:true
+  }
+}
+
+output containerRegistryId string = containerRegistry.id
+output containerRegistryHostName string = containerRegistry.properties.loginServer
