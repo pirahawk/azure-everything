d4f73419e6b53529c4128a2a2d13664aaa25480c
pirahawk <pirahawk@users.noreply.github.com>
2023-06-13 23:09:55 +0100

    Update Program.cs

--- az-container-apps/AzContainerAppService/AzContainerAppService/Program.cs
+++ az-container-apps/AzContainerAppService/AzContainerAppService/Program.cs
@@ -10,15 +10,15 @@ builder.Services.AddSwaggerGen();
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
+//if (app.Environment.IsDevelopment())
+//{
     app.UseSwagger();
     app.UseSwaggerUI();
-}
+//}
 
-app.UseHttpsRedirection();
+//app.UseHttpsRedirection();
 
-app.UseAuthorization();
+//app.UseAuthorization();
 
 app.MapControllers();
 
